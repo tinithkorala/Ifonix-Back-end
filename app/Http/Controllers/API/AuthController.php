66e7b73d5d0eef756988c5c87e18f3b15287d278
tinithk <tinithse@gmail.com>
2022-09-23 23:38:44 +0530

    developed to send token using sanctum

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,29 +4,49 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     
     public function register(Request $request) {
-
-        $validated = $request->validate([
+        
+        $validator = Validator::make($request->all(), [
             'name' => 'required|min:3|max:20|string',
             'email' => 'required|email|string|unique:users,email',
             'password' => 'required|string|confirmed|min:3'
         ]);
 
-        User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
-        ]);
+        if($validator->fails()) {
 
-        return response()->json([
-            'message' => 'Data saved !',
-        ], 201);
+            return response()->json([
+                'status' => 400,
+                'validation_errors' => $validator->messages(),
+            ]);
+
+        }else {
+
+            $validated = $validator->validated();
+
+            $user_obj = User::create([
+                'name'      => $validated['name'],  
+                'email'     => $validated['email'],
+                'password'  => Hash::make($validated['password']),
+            ]);
+
+            $user_token_string = $user_obj->createToken($user_obj->email.'_Token')->plainTextToken;
+    
+            return response()->json([
+                'status' => 201,
+                'user_name' => $user_obj->name,
+                'token' => $user_token_string,
+                'message' => 'User Registered Successfully !',
+            ]);
+
+        }
 
     }
 
