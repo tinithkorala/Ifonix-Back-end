af89d9a6fe68358aef7810348bd1c685a4560b9a
tinithk <tinithse@gmail.com>
2022-09-29 23:48:49 +0530

    document created for auth

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -13,6 +13,51 @@ use Illuminate\Support\Facades\Log;
 class AuthController extends Controller
 {
     
+    /**
+    * @OA\Post(
+    *   path="/api/register",
+    *   operationId="Register",
+    *   tags={"Authentication"},
+    *   summary="User Register",
+    *   description="User Register here",
+    *     @OA\RequestBody(
+    *         @OA\JsonContent(),
+    *         @OA\MediaType(
+    *            mediaType="multipart/form-data",
+    *            @OA\Schema(
+    *               type="object",
+    *               required={"name","email", "password", "password_confirmation"},
+    *               @OA\Property(property="name", type="text"),
+    *               @OA\Property(property="email", type="text"),
+    *               @OA\Property(property="password", type="password"),
+    *               @OA\Property(property="password_confirmation", type="password")
+    *            ),
+    *        ),
+    *    ),
+    *      @OA\Response(
+    *          response=201,
+    *          description="User Registered Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Register Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=422,
+    *          description="Unprocessable Entity",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=500,
+    *          description="Something went wrong",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(response=400, description="Bad request"),
+    *      @OA\Response(response=404, description="Resource Not Found"),
+    * )
+    */
     public function register(Request $request) {
         
         $validator = Validator::make($request->all(), [
@@ -65,6 +110,44 @@ class AuthController extends Controller
 
     }
 
+    /**
+    * @OA\Post(
+    * path="/api/login",
+    * operationId="login",
+    * tags={"Authentication"},
+    * summary="User Login",
+    * description="Login User Here",
+    *     @OA\RequestBody(
+    *         @OA\JsonContent(),
+    *         @OA\MediaType(
+    *            mediaType="multipart/form-data",
+    *            @OA\Schema(
+    *               type="object",
+    *               required={"email", "password"},
+    *               @OA\Property(property="email", type="email"),
+    *               @OA\Property(property="password", type="password")
+    *            ),
+    *        ),
+    *    ),
+    *      @OA\Response(
+    *          response=201,
+    *          description="Login Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Login Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=422,
+    *          description="Unprocessable Entity",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(response=400, description="Bad request"),
+    *      @OA\Response(response=404, description="Resource Not Found"),
+    * )
+    */
     public function login(Request $request) {
 
         $validator = Validator::make($request->all(),[
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="ABC Forum (Ifonix Test)", 
+ *      version="1.0",
+ *      @OA\Contact(
+ *          name="Tinith Korala",
+ *          email="tinithse@gmail.com"
+ *      )
+ *  )
+ * 
+ * 
+ * @OAS\SecurityScheme(
+ *      securityScheme="bearer_token",
+ *      type="http",
+ *      scheme="bearer"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
